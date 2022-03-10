e5733368087bb310a7760884b5f17c8a16e5366c
Simon <stauss.simon.01@gmail.com>
2022-03-10 22:37:59 +0100

    conversation handler comment

--- lib/handler/conversation_handler.dart
+++ lib/handler/conversation_handler.dart
@@ -1,16 +1,25 @@
+/// Handles the communication between app and backend
 class ConversationHandler {
-  static ConversationHandler? _instance;
+  // Private constructor
   ConversationHandler._();
 
+  /// The instance of this handler
+  static ConversationHandler? _instance;
+
+  /// Retrieve the instance of this singleton
   static ConversationHandler get instance {
     _instance ??= ConversationHandler._();
 
     return _instance!;
   }
 
+  /// Allow other components to get the instance by calling the constructor of this
+  /// handler
   factory ConversationHandler() => instance;
 
+  /// Ask the backend a question
   void askQuestion(String question) async {}
 
+  /// Send the answer of the user to the backend
   void sendAnswer(String answer) async {}
 }
