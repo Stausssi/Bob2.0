6bb658234a9beebccfdfd26920aac322a6afbf2d
Simon <stauss.simon.01@gmail.com>
2022-04-04 11:29:24 +0200

    code style bug fix

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -7,8 +7,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() async {
-  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized()
-      as IntegrationTestWidgetsFlutterBinding;
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   app.main();
 
