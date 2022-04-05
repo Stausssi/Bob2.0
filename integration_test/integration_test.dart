d1ba72ff2ff29fe2e9f3d75ad0911a03afafd1a2
Simon <stauss.simon.01@gmail.com>
2022-04-05 14:23:24 +0200

    Update integration_test.dart

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -2,7 +2,6 @@ import 'package:bob/chat/conversation.dart';
 import 'package:bob/main.dart' as app;
 import 'package:bob/main.dart';
 import 'package:bob/settings.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
@@ -29,31 +28,5 @@ void main() async {
     await tester.pumpAndSettle();
 
     expect(find.byType(Conversation), findsOneWidget);
-  });
-
-  testWidgets("Conversation test", (WidgetTester tester) async {
-    await tester.pumpAndSettle();
-
-    // We now should be in the conversation screen
-    // expect(
-    //   find.text("Bob ist bereit deine Fragen zu beantworten!"),
-    //   findsOneWidget,
-    // );
-
-    await tester.tap(find.text("Ich möchte schreiben"));
-    await tester.pumpAndSettle();
-
-    // Write a message
-    await tester.enterText(
-      find.byType(TextField),
-      "Diese Nachricht wurde vom Test formuliert",
-    );
-    await tester.tap(find.byIcon(Icons.send));
-    await tester.pumpAndSettle();
-
-    expect(
-      find.text("Diese Nachricht wurde vom Test formuliert"),
-      findsOneWidget,
-    );
   });
 }
