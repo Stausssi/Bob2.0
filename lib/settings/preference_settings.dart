bb84b793ee30ed64cb60e6b71a1aac70ac0bdae2
Simon <stauss.simon.01@gmail.com>
2022-04-07 09:38:48 +0200

    bug fix

--- lib/settings/preference_settings.dart
+++ lib/settings/preference_settings.dart
@@ -1,13 +1,12 @@
-import 'package:bob/Settings/settings.dart';
 import 'package:bob/api_keys.dart';
 import 'package:flutter/material.dart';
 import 'package:mapbox_search_flutter/mapbox_search_flutter.dart';
 import 'package:settings_ui/settings_ui.dart';
+
 import '../util.dart';
 
 class PreferenceSettings extends StatefulWidget {
   const PreferenceSettings({Key? key}) : super(key: key);
-
 
   @override
   _PreferenceSettingsState createState() => _PreferenceSettingsState();
